783844b9926dc8fd63384f19e4185fe5d93d70eb
Ricardo Bizerra <ricardo.bizerra19@gmail.com>
2023-04-22 15:57:26 -0300

    feature(entrada-timer): frequency counter

---
+++ nivel3/entrada_timer_controle/counter_freq/counter_freq.v
@@ -0,0 +1,17 @@
+module counter_freq(
+    input reg clk,
+    output reg clk_out
+);
+
+    reg [5:0] count = 6'b000000;
+    assign clk_out = 1'b0;
+
+    always @(posedge clk) begin
+        count = count + 1'b1;
+        if (count >= 50) begin
+            count = 6'b000000;
+            clk_out = !clk_out;
+        end
+    end
+
+endmodule
